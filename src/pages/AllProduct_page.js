7e0fa577c7311fd91363ccb4c32e1cf32d47f424
Tuhin Thakur <111550237+Tuhin810@users.noreply.github.com>
2023-08-04 23:03:55 +0530

    Update AllProduct_page.js

--- src/pages/AllProduct_page.js
+++ src/pages/AllProduct_page.js
@@ -6,7 +6,6 @@ import { useCart } from "../context/cart";
 import axios from "axios";
 import toast from "react-hot-toast";
 import Layout from "./../components/Layout/Layout";
-import { AiOutlineReload } from "react-icons/ai";
 import "../styles/Homepage.css";
 import Card from "./Card";
 import { Button } from "@material-tailwind/react";
@@ -235,7 +234,7 @@ const AllProducts = () => {
                 ) : (
                   <>
                     {" "}
-                    Loadmore <AiOutlineReload />
+                    Loadmore 
                   </>
                 )}
               </button>
